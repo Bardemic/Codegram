70d7b28da179049261fe917e83bcd828610ed5f5
Bardemic <134565367+Bardemic@users.noreply.github.com>
2024-11-16 13:59:01 -0600

    Add Basic Tutor Dashboard

---
+++ web/app/tutor/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import Header from "@/components/Header";
+import { useState } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Button } from "@/components/ui/button"
+  import { Separator } from "@/components/ui/separator"
+
+export default function TutorPage() {
+    //const [openTutees, setOpenTutees] = useState([{name: "Person 1", language: "Python"}, {name: "Person 2", language: "Javascript"}, {name: "Person 3", language: "C++"}]);
+    const [openTutees, setOpenTutees] = useState([])
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+          <Card className="w-64 h-80">
+            <CardHeader>Open Tutees</CardHeader>
+            <CardContent className="flex flex-col gap-4">
+                {openTutees.map((tutee) => {
+                    return(
+                        <div key={tutee.name}>
+                            <div className="flex flex-row justify-between">
+                                <div><p>{tutee.name}</p><p>{tutee.language}</p></div>
+                                <Button variant="outline" size="sm">View</Button>
+                            </div>
+                            <Separator className="my-2" />
+                        </div>
+                    )
+                })}
+                {openTutees.length === 0 && <p>No open tutees</p>}
+            </CardContent>
+          </Card>
+      </main>
+    </div>
+  );
+}
