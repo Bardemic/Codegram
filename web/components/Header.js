997c526486fcf8ee078997c229fa43b83a9e2d59
Bardemic <134565367+Bardemic@users.noreply.github.com>
2024-11-16 21:22:26 -0600

    Add Header Login/Logout

--- web/components/Header.js
+++ web/components/Header.js
@@ -2,11 +2,34 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { current } from "@/lib/ws";
 
-export default function EditorHeader() {
+function logout(router) {
+    try {
+        localStorage.removeItem("username");
+        localStorage.removeItem("passwordHash");
+        current.user = null;
+        router.push("/login");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default function Header() {
+    const [user, setUser] = useState(null);
+    const router = useRouter();
+    useEffect(() => {
+        try {
+            setUser(localStorage.getItem("username"));
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
   return (
     <header className="border-b shadow">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <div className="w-full mx-auto flex h-16 items-center justify-between px-10">
         <div className="flex items-center">
           <img
             src="/favicon.ico"
@@ -41,13 +64,17 @@ export default function EditorHeader() {
             Contact
           </a>
         </nav>
-        <div className="flex items-center space-x-4">
-          <Input placeholder="Search" className="w-36" />
-          <Button variant="outline" size="sm">
-            Sign Up
-          </Button>
-          <Button size="sm">Log In</Button>
-        </div>
+        {user ? (
+            <div className="flex justify-end items-center gap-4">
+                <p>{user}</p>
+                <Button variant="outline" onClick={() => logout(router)} size="sm">Log Out</Button>
+            </div>
+        ) : (
+            <div className="flex items-center space-x-4">
+                <Button variant="outline" size="sm">Sign Up</Button>
+                <Button size="sm">Log In</Button>
+            </div>
+        )}
       </div>
     </header>
   );
